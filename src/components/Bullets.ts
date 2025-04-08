3e5d7ec13a5ebb5011508efc4a3c6f508910236e
edpau <edpau@me.com>
2025-04-08 23:33:53 +0100

    feat: improve move function to handle all direction, refactor create bullet into constructor

--- src/components/Bullets.ts
+++ src/components/Bullets.ts
@@ -1,66 +1,168 @@
-import type { Pos, TankFireDirection} from "../game/types"
-import {delay} from "../utlity/helper"
+import type { Pos, TankFireDirection } from "../game/types";
+import { delay } from "../utlity/helper";
 
 export default class Bullet {
-    #pos: Pos;
-    #board:HTMLDivElement[][]
-    constructor(pos: Pos, board: HTMLDivElement[][] ) {
-      this.#pos = pos;
-      this.#board = board;
-    }
-  
-    create() {
-      this.#board[this.pos.y][this.pos.x].classList.add("bullet");
-      // bulletPool.push(this)
-    }
-  
-    async move(tankFireDirection: TankFireDirection) {
-      // let { x, y } = this.pos;
-      if (tankFireDirection === "up") {
-        if (this.#pos.y - 1 < 0) {
-          this.#board[this.#pos.y][this.#pos.x].classList.add("bullet");
-          await delay(20);
-          this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
-        } else if (
-          this.#board[this.#pos.y - 1][this.#pos.x].classList.contains(
-            "board__square--road"
-          )
-        ) {
-          await delay(20);
-          this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
-          this.#board[this.#pos.y - 1][this.#pos.x].classList.add("bullet");
-          this.#pos = { x: this.#pos.x, y: this.#pos.y - 1 };
-          this.move("up");
-        } else if (
-          this.#board[this.#pos.y - 1][this.#pos.x].classList.contains(
-            "board__square--wall"
-          )
-        ) {
-          this.#board[this.#pos.y - 1][this.#pos.x].classList.remove(
-            "board__square--wall"
-          );
-          this.#board[this.#pos.y - 1][this.#pos.x].classList.add(
-            "board__square--road"
-          );
-          this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
-        } else if (
-          this.#board[this.#pos.y - 1][this.#pos.x].classList.contains(
-            "board__square--boundary"
-          )
-        ) {
-          this.#board[this.#pos.y][this.#pos.x].classList.add("bullet");
-          await delay(20);
-          this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
-        }
+  #pos: Pos;
+  #board: HTMLDivElement[][];
+  readonly #DELAY: number = 100;
+
+  constructor(pos: Pos, board: HTMLDivElement[][]) {
+    this.#pos = pos;
+    this.#board = board;
+    this.#board[this.pos.y][this.pos.x].classList.add("bullet");
+  }
+
+
+// TODO refactor, too many repeat code
+  async move(tankFireDirection: TankFireDirection) {
+    if (tankFireDirection === "up") {
+      if (this.#pos.y - 1 < 0) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+      } else if (
+        this.#board[this.#pos.y - 1][this.#pos.x].classList.contains(
+          "board__square--road"
+        )
+      ) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+        this.#board[this.#pos.y - 1][this.#pos.x].classList.add("bullet");
+        this.#pos = { x: this.#pos.x, y: this.#pos.y - 1 };
+        this.move("up");
+      } else if (
+        this.#board[this.#pos.y - 1][this.#pos.x].classList.contains(
+          "board__square--wall"
+        )
+      ) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+        this.#board[this.#pos.y - 1][this.#pos.x].classList.remove(
+          "board__square--wall"
+        );
+        this.#board[this.#pos.y - 1][this.#pos.x].classList.add(
+          "board__square--road"
+        );
+      } else if (
+        this.#board[this.#pos.y - 1][this.#pos.x].classList.contains(
+          "board__square--boundary"
+        )
+      ) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+      }
+    } else if (tankFireDirection === "down") {
+      if (this.#pos.y + 1 >= this.#board.length) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+      } else if (
+        this.#board[this.#pos.y + 1][this.#pos.x].classList.contains(
+          "board__square--road"
+        )
+      ) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+        this.#board[this.#pos.y + 1][this.#pos.x].classList.add("bullet");
+        this.#pos = { x: this.#pos.x, y: this.#pos.y + 1 };
+        this.move("down");
+      } else if (
+        this.#board[this.#pos.y + 1][this.#pos.x].classList.contains(
+          "board__square--wall"
+        )
+      ) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+        this.#board[this.#pos.y + 1][this.#pos.x].classList.remove(
+          "board__square--wall"
+        );
+        this.#board[this.#pos.y + 1][this.#pos.x].classList.add(
+          "board__square--road"
+        );
+      } else if (
+        this.#board[this.#pos.y + 1][this.#pos.x].classList.contains(
+          "board__square--boundary"
+        )
+      ) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+      }
+    } else if (tankFireDirection === "left") {
+      if (this.#pos.x - 1 < 0) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+      } else if (
+        this.#board[this.#pos.y][this.#pos.x - 1].classList.contains(
+          "board__square--road"
+        )
+      ) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+        this.#board[this.#pos.y][this.#pos.x - 1].classList.add("bullet");
+        this.#pos = { x: this.#pos.x - 1, y: this.#pos.y };
+        this.move("left");
+      } else if (
+        this.#board[this.#pos.y][this.#pos.x - 1].classList.contains(
+          "board__square--wall"
+        )
+      ) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+        this.#board[this.#pos.y][this.#pos.x - 1].classList.remove(
+          "board__square--wall"
+        );
+        this.#board[this.#pos.y][this.#pos.x - 1].classList.add(
+          "board__square--road"
+        );
+      } else if (
+        this.#board[this.#pos.y][this.#pos.x - 1].classList.contains(
+          "board__square--boundary"
+        )
+      ) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+      }
+    } else if (tankFireDirection === "right") {
+      if (this.#pos.x + 1 >= this.#board[0].length) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+      } else if (
+        this.#board[this.#pos.y][this.#pos.x + 1].classList.contains(
+          "board__square--road"
+        )
+      ) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+        this.#board[this.#pos.y][this.#pos.x + 1].classList.add("bullet");
+        this.#pos = { x: this.#pos.x + 1, y: this.#pos.y };
+        this.move("right");
+      } else if (
+        this.#board[this.#pos.y][this.#pos.x + 1].classList.contains(
+          "board__square--wall"
+        )
+      ) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
+        this.#board[this.#pos.y][this.#pos.x + 1].classList.remove(
+          "board__square--wall"
+        );
+        this.#board[this.#pos.y][this.#pos.x + 1].classList.add(
+          "board__square--road"
+        );
+      } else if (
+        this.#board[this.#pos.y][this.#pos.x + 1].classList.contains(
+          "board__square--boundary"
+        )
+      ) {
+        await delay(this.#DELAY);
+        this.#board[this.#pos.y][this.#pos.x].classList.remove("bullet");
       }
     }
-  
-    destory() {
-      // create bullet pool array or bullet list
-    }
-  
-    get pos() {
-      return this.#pos;
-    }
   }
-  
+
+  destory() {
+    // create bullet pool array or bullet list
+  }
+
+  get pos() {
+    return this.#pos;
+  }
+}
