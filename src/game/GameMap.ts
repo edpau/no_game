a9fa046de2e4f2b0a194a387201b87f729db7237
edpau <edpau@me.com>
2025-04-11 12:38:41 +0100

    feat: add flag to game map and implement flag position logic in GameMap class

--- src/game/GameMap.ts
+++ src/game/GameMap.ts
@@ -1,3 +1,5 @@
+import { Pos } from "./types";
+
 const gameMap: number[][] = [
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -5,24 +7,44 @@ const gameMap: number[][] = [
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 2, 0, 0, 0, 0, 1],
-  [1, 0, 0, 0, 2, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 2, 3, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
 
+// throw error if there is more than one 3 in the gameMap
+
 const squareClassNames: Record<number, string> = {
   0: "board__square--road",
   1: "board__square--boundary",
   2: "board__square--wall",
+  3: "board__square--flag",
 };
 
 export default class GameMap {
   #board: HTMLDivElement[][] = [];
   #boardEl: HTMLDivElement;
+  #flag: Pos;
 
   constructor(boardEl: HTMLDivElement) {
     this.#boardEl = boardEl;
+
+    try {
+      this.#flag = this.flagPos(gameMap);
+    } catch (error) {
+      // TODO take away this line in production
+      console.error("Map initialization failed:", error);
+
+      const errorMsg = document.createElement("div");
+      if (error instanceof Error) {
+        errorMsg.textContent = error.message;
+        errorMsg.style.color = "red";
+        boardEl.appendChild(errorMsg);
+      }
+
+      throw error;
+    }
     this.#board = this.create(this.#boardEl);
   }
 
@@ -52,8 +74,32 @@ export default class GameMap {
     return this.#board;
   }
 
+  get flag() {
+    return this.#flag;
+  }
+
   reset() {
     this.#boardEl.innerHTML = "";
     this.#board = this.create(this.#boardEl);
+  }
+
+  flagPos(gameMap: number[][]): Pos {
+    let flagCount = 0;
+    let pos: Pos | null = null;
+
+    for (let y = 0; y < gameMap.length; y++) {
+      const x = gameMap[y].indexOf(3);
+      if (x > -1) {
+        flagCount++;
+        if (flagCount > 1) {
+          throw new Error("gameMap has more than one flag");
+        }
+        pos = { x, y };
+      }
+    }
+    if (flagCount === 0 || pos === null) {
+      throw new Error("gameMap does not have a flag");
+    }
+    return pos;
   }
 }
