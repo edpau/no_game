bf84c1cb1d96441e1717e8917b0b4c193f63b429
edpau <edpau@me.com>
2025-04-14 22:30:33 +0100

    refactor: remove unused type and correct typo from types.ts

--- src/game/types.ts
+++ src/game/types.ts
@@ -1,10 +1,8 @@
-export type TankFireDirection = "up" | "down" | "left" | "right";
-
 export type Pos = { x: number; y: number };
 
 export enum Direction {
     UP = "up",
     DOWN = "down",
     LEFT = "left",
-    RIGht = "right",
+    RIGHT = "right",
   }
